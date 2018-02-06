fbe90aaa475019d248021b4d6b120cd10461c749
Bradley Norris <bradnor@Bradley-Norriss-MacBook-Pro.local>
2018-02-05 18:48:03 -0800

    fixed camera accee on master branch.

--- static/JS/camera.js
+++ static/JS/camera.js
@@ -1,4 +1,4 @@
-Webcam.set({
+let webCam = Webcam.set({
     width: 400,
     height: 300,
     image_format: 'jpeg',
@@ -8,12 +8,8 @@ Webcam.attach('#mywebcam');
 
 function takescreenshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById('results').innerHTML = "<h2>Here is your image:</h2><img width='400' src='"+data_uri+"'>";
-    });
+        document.getElementById('results').innerHTML = "<img width='400' src='"+data_uri+"'><h3>Your Picture</h3>";
+        document.getElementById('mywebcam').style.display = "none";
+        document.getElementById('camera-button').style.display = "none";
+    })
 };
-
-function retake() {
-    if(takescreenshot){
-        
-    }
-}
