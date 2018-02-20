5432c4375648e1d834b92002c95351c71ffda42c
Javier Ramirez <apollo.infinity88@gmail.com>
2018-02-19 19:40:22 -0800

    Made the default template created by Eric the default page when flask is ran

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +7,4 @@ def index():
 
 if __name__ == '__main__':
     # app.secret_key='secret123'
-    app.run(debug = True)
+    app.run(debug = True, threaded=True)
